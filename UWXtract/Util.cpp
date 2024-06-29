936b41a6ef2ec4eb81d1199d2ec9af42c5c93070
Draxinusom <63679864+Draxinusom@users.noreply.github.com>
2024-06-29 14:18:49 -0400

    Partial transparency support
    Emphasis on partial

--- UWXtract/Util.cpp
+++ UWXtract/Util.cpp
@@ -86,7 +86,8 @@ void GetPalette24(
 void GetPalette32(
 	const std::string UWPath,
 	const unsigned int PaletteIndex,
-	char PaletteBuffer[256 * 4]
+	char PaletteBuffer[256 * 4],
+	bool IncludePartialTransparency
 ) {
 	FILE* PALS = fopen((UWPath + "\\DATA\\PALS.DAT").c_str(), "rb");
 	char TempBuffer[256 * 3];
@@ -111,16 +112,108 @@ void GetPalette32(
 		TempBuffer[(c * 3) + 2] = RedTemp;
 	}
 
-// Now populate return buffer, inserting alpha byte
+// Now build return buffer, inserting alpha byte
 	for (int c = 0; c < 256; c++) {
 		PaletteBuffer[(c * 4) + 0] = TempBuffer[(c * 3) + 0];
 		PaletteBuffer[(c * 4) + 1] = TempBuffer[(c * 3) + 1];
 		PaletteBuffer[(c * 4) + 2] = TempBuffer[(c * 3) + 2];
-	// Add alpha byte -- Only first color should be alpha
+	// Add alpha byte -- First color is always alpha (maybe?)
 		PaletteBuffer[(c * 4) + 3] = (c == 0 ? 0x00 : 0xFF);
 	}
-}
 
+// Last, attempt to recreate partial transparency from XFER.DAT for palette 0 (if flag set)
+/***
+	This really isn't possible to do with true accuracy as partial transparency in Ultima Underworld works by looking at each
+	pixel that is being covered by the partially transparent object/critter and mapping that pixel's color to a different one
+	using a mapping table in XFER.DAT
+
+	This means that when exporting a standalone image with no background, it's not really possible to generate an image that,
+	when overlayed over another image (like a floor or wall texture), will match exactly with what the same object/critter
+	would look like in game since the resulting color would have to be defined in the index, making the original devs have to
+	just pick whatever color was close enough for the look they were going for.  Additionally, since they were just mapping to
+	a different color, they were able to pick a final color that would be mathematically impossible to generate with an overlay
+	(at least one with a consistent alpha) or would have to be radically different from one color pairing to the next.
+
+	As there's no precise way to do this, here's how the colors used were determined:
+		Black:	While technically wrong (maybe?), went with 1 step above flat black to make it have partial visibility over black
+		White:	Went with the whitest color available in both indexes as I don't think it is ever used for textures so should always stand out
+		Red:	Took the two red colors that did not change (33-34) and averaged them
+		Green:  Took the three green colors that did not change (174-176) and averaged them 
+		Blue:	Took the two blue colors in the primary blue range (there's a few scattered dark blues that don't change that I ignored) that did not change (78-79) and averaged them (Index 12 is mapped to heavily but I thought it far too dark when used as an overlay)
+		Alpha:	A mix of testing to see how close various values hit to target colors while still being (at least somewhat) visible across the board
+
+	Additionally, since the palette colors are 6 bits (0-63) making all colors multiples of 4, I shifted the averaged value to match that
+	i.e.  For blue, the two colors used were:
+		78:  60 60 252
+		79:   0  0 252
+	Making an average of 30 for red/green.  That was shifted down to 28 to make them fit as 6 bit colors.
+***/
+	if (IncludePartialTransparency && PaletteIndex == 0) {
+	// UW1
+		if (PaletteBuffer[0] == 0x04) { // Can identify this is UW1 by the alpha color
+		// 0-FB/251 -- Black
+			PaletteBuffer[1004] = 0x04u;	// B: 4
+			PaletteBuffer[1005] = 0x04u;	// G: 4
+			PaletteBuffer[1006] = 0x04u;	// R: 4
+			PaletteBuffer[1007] = 0xA4u;	// A: 164
+		// 1-FC/252 -- White
+			PaletteBuffer[1008] = 0xFCu;	// B: 252
+			PaletteBuffer[1009] = 0xFCu;	// G: 252
+			PaletteBuffer[1010] = 0xFCu;	// R: 252
+			PaletteBuffer[1011] = 0x28u;	// A: 40
+		/***
+			Note:
+			XFER.DAT has 6 mappings in UW1 (assuming I'm understanding the file right) but only 0 & 1 appear to be used
+
+			I've done some scanning for usage of color indexes in that range (F0-FF) across all GR & CRIT images and don't
+			see anywhere that they could be used though I could be wrong/missing something
+
+			For reference:
+				2:	Unknown - slightly darkens the colors (far less than 0) and _usually_ pushes them to a more red color
+				3:	Green
+				4:	Blue
+				5:	Red
+		***/
+		}
+	// UW2
+		else {
+		// 0-F9/249 -- Red 
+			PaletteBuffer[996] = 0x1Cu;	// B: 28
+			PaletteBuffer[997] = 0x0Cu;	// G: 12
+			PaletteBuffer[998] = 0xC4u;	// R: 196
+			PaletteBuffer[999] = 0x68u;	// A: 104
+		// 1-FA/250 -- Green
+			PaletteBuffer[1000] = 0x10u;	// B: 16
+			PaletteBuffer[1001] = 0x50u;	// G: 80
+			PaletteBuffer[1002] = 0x28u;	// R: 40
+			PaletteBuffer[1003] = 0x80u;	// A: 128
+		// 2-FB/251 -- Blue
+			PaletteBuffer[1004] = 0xFCu;	// B: 252
+			PaletteBuffer[1005] = 0x1Cu;	// G: 28
+			PaletteBuffer[1006] = 0x1Cu;	// R: 28
+			PaletteBuffer[1007] = 0x68u;	// A: 104
+		// 3-FC/252 -- White
+			PaletteBuffer[1008] = 0xFCu;	// B: 252
+			PaletteBuffer[1009] = 0xFCu;	// G: 252
+			PaletteBuffer[1010] = 0xFCu;	// R: 252
+			PaletteBuffer[1011] = 0x28u;	// A: 40
+		// 4-FD/253 -- Black
+			PaletteBuffer[1012] = 0x04u;	// B: 4
+			PaletteBuffer[1013] = 0x04u;	// G: 4
+			PaletteBuffer[1014] = 0x04u;	// R: 4
+			PaletteBuffer[1015] = 0xA4u;	// A: 164
+		/***
+			Note:
+			Color index FE/254 has _something_ done with it that is similar to what is being done here but not
+			clear how that mapping works and I don't think that it's really aiming for pseudo transparency either.
+
+			Since it _does_ show normally at times and the logic for when it changes is unknown (by me), it's left untouched.
+
+			Primarily used by CR26_02_Spectre attack animations
+		***/
+		}
+	}
+}
 
 // Returns all standard palettes, reformatted for export -- Only usage is BYTXtract which is 24-bit only so no alpha byte
 void GetPaletteAll(
